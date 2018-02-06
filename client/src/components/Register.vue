0d3318433aec02381cc895dab5b76a5136ae8871
GiL <iamleeyounggil@gmail.com>
2018-02-06 15:48:46 -0500

    Display result of user data authentications from serverside in register page

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -14,6 +14,8 @@
         v-model="password"
         placeholder="password"/>
       <br/>
+      <div class="error" v-html="error"/>
+      <br/>
       <button
         @click="register">
         Register</button>
@@ -28,17 +30,21 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: null
 
     }
   },
   methods: {
     async register() {
+      try{
         await AuthenticationService.register({
           email: this.email,
           password: this.password
         })
-
+      }catch(error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
@@ -46,5 +52,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .error {
+    color: red;
+  }
 
 </style>
